235a558a4f7070e22637d665973e6c5577883c35
Athos-0day <athos.0day@protonmail.com>
2024-12-20 23:29:26 +0100

    20-12-24

---
+++ Transposition_Cipher/EncryptTranspo.py
@@ -0,0 +1,30 @@
+#Implementation of encryption by transposition
+
+#Retrieving values
+print("Give a key value and the associated message")
+Key = int(input("The value of the key: "))
+Message = input("The message you want to encrypt: ")
+
+#variable initialization 
+Length = len(Message)
+k = 0
+n = 0
+EncryptMessage = ""
+
+while n<Key :
+    while k<=(Length//Key) :
+        print(n+k*Key)
+        if (n+k*Key)<Length :
+            EncryptMessage += Message[n+k*Key]
+        k+=1
+        
+    k=0
+    n+=1
+
+print(EncryptMessage)
+
+
+
+
+
+
