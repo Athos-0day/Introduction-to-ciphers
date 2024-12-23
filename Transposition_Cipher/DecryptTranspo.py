9ca9504bdb22315deaa2d3645155460419b81bf2
Athos-0day <athos.0day@protonmail.com>
2024-12-23 08:45:45 +0100

    23-12-24

---
+++ Transposition_Cipher/DecryptTranspo.py
@@ -0,0 +1,28 @@
+#Decryption of the transposition cipher
+
+#Retrieve the information
+print("Give a key value and the associated ciphertext")
+Key = int(input("The value of the key: "))
+CipherText = input("The message you want to decrypt: ")
+
+#Initialisation of the values
+Length = len(CipherText)
+Rows = 0
+Columns = 0
+Novalue = Key*((Length//Key)+1) - Length
+PlainText = [""] * ((Length//Key)+1)
+
+for Caracters in CipherText :
+    PlainText[Columns] += Caracters
+    Columns += 1
+
+    if Columns==((Length//Key)+1) or (Columns==(Length//Key) and Rows>=(Key-Novalue)) :
+        Columns = 0 
+        Rows +=1
+
+Message = ""
+for val in PlainText :
+    Message += val
+
+print("The plaintext is : ")
+print(Message)
